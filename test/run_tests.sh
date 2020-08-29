3db786f93253ddd4b753836040e8601dfb0a1711
Captain James Tiberius Kirk <ed@intelligentdatadesign.com>
2020-08-29 15:21:30 -0600

    developing testing

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,3 +1,11 @@
+# This is part of STAREmaster. This is a script to run tests.
 set -e
+set -x
+
+# Create a sidecar file.
 ../src/createSidecarFile data/MOD05_L2.A2005349.2125.061.2017294065400.hdf
-#../src/createSidecarFile -d MOD09 data/MYD09.A2020058.1515.006.2020060020205.hdf
+
+# Create a CDL file of the header info from the STARE index sidecar
+# file we produced.
+
+
