ea88cfc6c7ce3b109abf164da4dfe21c41125685
Captain James Tiberius Kirk <ed@intelligentdatadesign.com>
2020-08-29 16:06:44 -0600

    fixing ref files

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -9,6 +9,6 @@ echo "*** creating CDL of MOD05 sidecar file header..."
 ncdump -h data/MOD05_L2.A2005349.2125.061.2017294065400_stare.nc > MOD05_L2.A2005349.2125.061.2017294065400_stare_out.cdl
 
 echo "*** checking that sidecar header is correct..."
-diff -b -w MOD05_L2.A2005349.2125.061.2017294065400_stare_out.cdl MOD05_L2.A2005349.2125.061.2017294065400_stare.cdl
+diff -b -w MOD05_L2.A2005349.2125.061.2017294065400_stare_out.cdl ref_MOD05_L2.A2005349.2125.061.2017294065400_stare.cdl
 
 
