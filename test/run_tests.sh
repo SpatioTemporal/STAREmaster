7a2de201a6666f082b26d974944d505852165e1f
Captain James Tiberius Kirk <ed@intelligentdatadesign.com>
2020-08-29 15:51:41 -0600

    now checking header info for large file tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,8 +5,6 @@ set -x
 echo "*** creating sidecar file for MOD05..."
 ../src/createSidecarFile data/MOD05_L2.A2005349.2125.061.2017294065400.hdf
 
-# Create a CDL file of the header info from the STARE index sidecar
-# file we produced.
 echo "*** creating CDL of MOD05 sidecar file header..."
 ncdump -h data/MOD05_L2.A2005349.2125.061.2017294065400_stare.nc > MOD05_L2.A2005349.2125.061.2017294065400_stare_out.cdl
 
