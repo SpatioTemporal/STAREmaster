d049efe8051feeb14db8569bc859c525016a3b83
Captain James Tiberius Kirk <ed@intelligentdatadesign.com>
2020-09-06 12:20:56 -0600

    starting to develop MOD09GA testing and code

--- src/createSidecarFile.cpp
+++ src/createSidecarFile.cpp
@@ -9,6 +9,7 @@
 #include "ssc.h"
 #include "ModisL2GeoFile.h"
 #include "Modis09L2GeoFile.h"
+#include "Modis09GAGeoFile.h"
 #include "NetcdfSidecarFile.h"
 
 using namespace std;
@@ -107,6 +108,7 @@ main(int argc, char *argv[])
     NetcdfSidecarFile sf;
     string file_out;
     const std::string MOD09 = "MOD09";
+    const std::string MOD09GA = "MOD09GA";
     vector<string> stare_index_name;
 
     // Input file must be provided.
@@ -126,6 +128,12 @@ main(int argc, char *argv[])
     {
 	gf = new Modis09L2GeoFile();
 	if (((Modis09L2GeoFile *)gf)->readFile(argv[optind], arg.verbose, arg.quiet, arg.build_level))
+	    return 99;
+    }
+    else if (arg.data_type == MOD09GA)
+    {
+	gf = new Modis09GAGeoFile();
+	if (((Modis09GAGeoFile *)gf)->readFile(argv[optind], arg.verbose, arg.quiet, arg.build_level))
 	    return 99;
     }
     else
