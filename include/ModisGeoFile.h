4ea92e2205113e78998455f834323f9b2c68e759
Captain James Tiberius Kirk <ed@intelligentdatadesign.com>
2021-02-21 13:11:53 -0700

    adding ModisGeoFile

---
+++ include/ModisGeoFile.h
@@ -0,0 +1,30 @@
+/// @file
+
+/// This class contains the main functionality for reading a file that
+/// may be given a STARE sidecar file.
+
+/// Ed Hartnett 4/4/20
+
+#ifndef MODIS_GEO_FILE_H_ /**< Protect file from double include. */
+#define MODIS_GEO_FILE_H_
+
+#include "GeoFile.h"
+#include <mfhdf.h>
+#include <hdf.h>
+#include <HdfEosDef.h>
+
+using namespace std;
+
+/**
+ * This is the base class for a Modis data file with geolocation.
+ */
+class ModisGeoFile : public GeoFile
+{
+public:
+    ModisGeoFile();
+    ~ModisGeoFile();
+    
+    int determineFormat(const std::string fileName, int *gf_format);
+};
+
+#endif /* MODIS_GEO_FILE_H_ */
---
+++ src/ModisGeoFile.cpp
@@ -0,0 +1,66 @@
+/// @file
+/// THis is the root class for the STAREmaster functionality. Derived
+/// classes read different format input files and create sidecar files
+/// for them.
+/// 
+/// Ed Hartnett 2/21/21
+
+#include "config.h"
+#include "ModisGeoFile.h"
+#include "SidecarFile.h"
+#include <netcdf.h>
+
+/** Construct a ModisGeoFile.
+ *
+ * @return a ModisGeoFile
+ */
+ModisGeoFile::ModisGeoFile()
+{
+    cout<<"ModisGeoFile constructor\n";
+
+}
+
+/** Destroy a ModisGeoFile.
+ *
+ */
+ModisGeoFile::~ModisGeoFile()
+{
+    cout<<"ModisGeoFile destructor\n";
+
+}
+
+/**
+ * Determine the format of the target file.
+ *
+ * @param fileName Name of the target file.
+ * @param gf_format Pointer to int that gets a constant indicating the
+ * format. Ignored if NULL.
+ *
+ * @return 0 for success, error code otherwise.
+ */
+int
+ModisGeoFile::determineFormat(const std::string fileName, int *gf_format)
+{
+    int32 swathfileid;
+    
+    if (gf_format)
+	*gf_format = SSC_FORMAT_HDF4;
+
+    // Try to open as swath file with the HDF-EOS library.
+    swathfileid = SWopen((char *)fileName.c_str(), DFACC_RDONLY);
+    if (swathfileid != -1)
+    {
+	if (gf_format)
+	    *gf_format = SSC_FORMAT_MODIS_L2;	
+
+	// Close the swath file.
+	if (SWclose(swathfileid) < 0)
+	    return SSC_EHDF4ERR;
+    }
+    
+    return 0;
+}
+
+
+
+
